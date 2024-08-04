96286deaee4f6e5b66f5460302f02a97af66a901
W. Meijer <wouter.meijer@tno.nl>
2024-08-04 20:59:50 +0200

    fix: move single game logic to own function

--- src/lib.rs
+++ src/lib.rs
@@ -49,30 +49,35 @@ fn update_correctly_guessed_letters(config: &Config, player_state: &mut PlayerSt
     }
 }
 
+fn run_game(config: &Config) {
+    let mut state: GameState = GameState::PLAYING;
+    let mut player_state = PlayerState::build(config);
+
+    while state == GameState::PLAYING {
+        state = run_game_loop(config, crate::cli::take_guess(), &mut player_state);
+
+        if state == GameState::LOST {
+            crate::cli::print_loss()
+        }
+
+        if state == GameState::WON {
+            crate::cli::print_win()
+        }
+    }
+}
+
 pub fn run(config: &Config) -> Result<(), Box<dyn Error>> {
     let mut rematch = true;
 
     while rematch == true {
-        let mut state: GameState = GameState::PLAYING;
-        let mut player_state = PlayerState::build(config);
-
-        while state == GameState::PLAYING {
-            state = run_game_loop(config, crate::cli::take_guess(), &mut player_state);
-
-            if state == GameState::LOST {
-                crate::cli::print_loss()
-            }
-
-            if state == GameState::WON {
-                crate::cli::print_win()
-            }
-        }
+        run_game(config);
 
         let rematch_input = crate::cli::take_user_input("Do you want a rematch? (yes/no)");
         if rematch_input.contains("n") {
             rematch = false
         }
     }
+
     Ok(())
 }
 
