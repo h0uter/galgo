2179449aea029823d61a67c06b1cb0ffede9488e
W. Meijer <wouter.meijer@tno.nl>
2024-08-04 17:51:54 +0200

    feat: add lives to gameplay. also update gameplay print statements with emojis.

--- src/lib.rs
+++ src/lib.rs
@@ -19,9 +19,23 @@ fn take_guess() -> char {
         .expect("More than one character was guessed.");
 }
 
-fn run_game_loop(config: &mut Config, input: char) -> GameState {
+struct PlayerState {
+    pub wrong_guesses: u32,
+}
+
+fn run_game_loop(config: &mut Config, input: char, player_state: &mut PlayerState) -> GameState {
     if !config.secret_word.contains(input) {
-        return GameState::LOST;
+        player_state.wrong_guesses += 1;
+
+        if player_state.wrong_guesses > config.lives {
+            return GameState::LOST;
+        } else {
+            println!(
+                "WRONG... lives remaining {}",
+                config.lives - player_state.wrong_guesses
+            );
+            return GameState::PLAYING;
+        }
     }
 
     // check for hits of guess in solution
@@ -38,28 +52,31 @@ fn run_game_loop(config: &mut Config, input: char) -> GameState {
             .replace_range(hit_idx..hit_idx + 1, &input.to_string());
     }
 
-    println!("status: {}", config.correctly_guessed_letters);
+    println!("CORRECT... status: {}", config.correctly_guessed_letters);
 
     if config.correctly_guessed_letters == config.secret_word {
         return GameState::WON;
     }
 
+    // return GameState::PLAYING;
     return GameState::PLAYING;
 }
 
 pub fn run(config: &mut Config) -> Result<(), Box<dyn Error>> {
-    println!("{}", config.correctly_guessed_letters);
-
     let mut state: GameState = GameState::PLAYING;
+    let mut player_state = PlayerState { wrong_guesses: 0 };
+
     while state == GameState::PLAYING {
-        state = run_game_loop(config, take_guess());
+        state = run_game_loop(config, take_guess(), &mut player_state);
 
         if state == GameState::LOST {
-            println!("too bad peanut butter");
+            println!("");
+            println!("too bad peanut butter... YOU LOST!");
         }
 
         if state == GameState::WON {
-            println!("you win!");
+            println!("");
+            println!("🎉🎉🎉 YOU WIN! 🎉🎉🎉");
         }
     }
 
@@ -69,16 +86,19 @@ pub fn run(config: &mut Config) -> Result<(), Box<dyn Error>> {
 pub struct Config {
     pub secret_word: String,
     pub correctly_guessed_letters: String,
+    pub lives: u32,
 }
 
 impl Config {
     pub fn build() -> Config {
         let secret_word = get_user_input("Provide your secret word:");
         let correctly_guessed_letters = "_".repeat(secret_word.len());
+        let lives = 3;
 
         return Config {
             secret_word,
             correctly_guessed_letters,
+            lives,
         };
     }
 }
