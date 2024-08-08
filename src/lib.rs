3c03e61737de56c20842ec6d40595e3de65170bd
BoB <bobluppes@gmail.com>
2024-08-08 21:17:39 +0200

    do all communication between guessing player and game master loops through session

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@ use std::error::Error;
 // config
 pub mod config;
 pub use crate::config::Config;
+use crate::config::game_session;
 
 // CLI input output
 mod cli;
@@ -9,24 +10,34 @@ mod cli;
 // game core
 
 // This function will be the concern of the "word master" in a later increment
-fn check_guess(config: &Config, player_state: &mut PlayerState, guess: char) -> bool {
-    if !config.secret_word.contains(guess) {
-        player_state.wrong_guesses += 1;
-        return false;
+fn check_guess(config: &Config) {
+    let guess = config.session.borrow_mut().receive_guess().guess;
+
+    let mut was_correct = false;
+    let mut hit_idxs: Vec<usize> = Vec::new();
+
+    if config.secret_word.contains(guess) {
+        was_correct = true;
+        hit_idxs = determine_hit_idxs(config, guess);
     }
 
-    update_correctly_guessed_letters(config, player_state, guess);
-    return true;
+    config.session.borrow_mut().submit_result(game_session::Result{was_correct, hit_idxs});
 }
 
 // This function and the print_and_determine function below will be the concern of
 // the "guessing player" in a later increment.
-fn take_guessing_player_guess() -> char {
-    cli::take_guess()
+fn take_guessing_player_guess(config: &Config) -> char {
+    let guess = cli::take_guess();
+    config.session.borrow_mut().submit_guess(game_session::Guess{guess});
+    return guess;
 }
 
-fn print_and_determine_game_state(config: &Config, player_state: &mut PlayerState, correct_guess: bool) -> GameState {
-    if !correct_guess {
+fn print_and_determine_game_state(config: &Config, player_state: &mut PlayerState, guess: char) -> GameState {
+    let result= config.session.borrow_mut().receive_result();
+    write_correct_characters(player_state, result.hit_idxs, guess);
+
+    if !result.was_correct {
+        player_state.wrong_guesses += 1;
         cli::print_hangman_stage(player_state.wrong_guesses + (6 - config.lives));
 
         if player_state.wrong_guesses >= config.lives {
@@ -47,20 +58,25 @@ fn print_and_determine_game_state(config: &Config, player_state: &mut PlayerStat
 }
 
 fn run_game_loop(config: &Config, player_state: &mut PlayerState) -> GameState {
-    let guess = take_guessing_player_guess();
-    let correct_guess = check_guess(config, player_state, guess);
-    return print_and_determine_game_state(config, player_state, correct_guess);
+    // Guessing player
+    let guess = take_guessing_player_guess(config);
+
+    // Word master
+    check_guess(config);
+
+    // Guessing player
+    return print_and_determine_game_state(config, player_state, guess);
 }
 
-fn update_correctly_guessed_letters(config: &Config, player_state: &mut PlayerState, guess: char) {
-    // check for hits of guess in solution
-    let hit_idxs: Vec<usize> = config
+fn determine_hit_idxs(config: &Config, guess: char) -> Vec<usize> {
+    return config
         .secret_word
         .char_indices()
         .filter_map(|(i, c)| if c == guess { Some(i) } else { None })
         .collect();
+}
 
-    // Fill in the correctly guessed letters in the user facing message
+fn write_correct_characters(player_state: &mut PlayerState, hit_idxs: Vec<usize>, guess: char) {
     for hit_idx in hit_idxs {
         player_state
             .correctly_guessed_letters
