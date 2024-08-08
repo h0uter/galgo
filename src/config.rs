d92d54c9c9a1ca83b516d5af19a7310871dc5d7a
BoB <bobluppes@gmail.com>
2024-08-08 20:39:03 +0200

    add pointer to session in config

--- src/config.rs
+++ src/config.rs
@@ -1,11 +1,15 @@
+pub mod game_session;
+use game_session::Session;
+
 pub struct Config {
     // Static configuration we take at the start
     pub secret_word: String,
     pub lives: usize,
+    pub session: Box<dyn Session>,
 }
 
 impl Config {
-    pub fn build() -> Config {
+    pub fn build(session: Box<dyn Session>) -> Config {
         let secret_word = crate::cli::take_user_input("Provide your secret word:");
         let lives = 3; // cannot be larger than seven
 
@@ -13,6 +17,6 @@ impl Config {
             panic!("lives cannot be larger than 7, we dont have more hangman drawings.")
         }
 
-        return Config { secret_word, lives };
+        return Config { secret_word, lives, session };
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 use std::process;
 
+use galgo::config::game_session::local_session::LocalSession;
+
 fn main() {
     // Building this config will be the concern of the word master in a future increment
-    let mut game_config = galgo::Config::build();
+    let local_session = Box::new(LocalSession::build());
+    let mut game_config = galgo::Config::build(local_session);
 
     if let Err(e) = galgo::run(&mut game_config) {
         eprintln!("Application error {e}");
